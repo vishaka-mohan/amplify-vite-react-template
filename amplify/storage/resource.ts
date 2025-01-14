8d9ba8fdfe59a93704c712f86ab84543b203cf4a
vishaka-mohan <vmohan0211@gmail.com>
2025-01-14 14:00:02 +0530

    change resource storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,43 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'gameDialogueAudios',
   access: (allow) => ({
-    '/*': [
+    'scene1/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene2/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene3/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene4/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene5/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene6/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene8/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene10/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene11/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+    'scene12/*': [
       allow.authenticated.to(['read','write']),
       allow.guest.to(['read', 'write'])
     ],
